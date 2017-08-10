a813f97addcf2bab750fdbb529c8ad56dc7fc99b
Ryan Collins <RyanCCollins@users.noreply.github.com>
2017-08-09 20:47:20 -0400

    Update root_query_type.rb

--- app/graph/types/root_query_type.rb
+++ app/graph/types/root_query_type.rb
@@ -3,5 +3,5 @@ RootQueryType = GraphQL::ObjectType.define do
   description 'The root level query type'
 
   field :users, types[UserType], field: FieldGenerator::CreateField.all(type: types[UserType], model: User)
-  field :user, types[UserType], field: FieldGenerator::CreateField.one(type: UserType, model: User)
+  field :user, UserType, field: FieldGenerator::CreateField.one(type: UserType, model: User)
 end
