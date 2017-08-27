348f9980c242c7490e81aa6fd96b8cf349c5ed39
Ryan Collins <RyanCCollins@users.noreply.github.com>
2017-08-26 21:21:23 -0400

    Update user_type.rb

--- app/graph/types/user_type.rb
+++ app/graph/types/user_type.rb
@@ -1,6 +1,6 @@
 UserType = GraphQL::ObjectType.define do
   name 'User'
-  description 'The public user type which does not include any authorized properties.'
+  description 'The public user type which does not include any properties that require authentication.'
   field :id, !types.ID, 'The id of the user'
   field :name, !types.String, 'The name of the user'
   field :bio, types.String, 'The bio of the user'
